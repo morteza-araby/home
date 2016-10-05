5cedae48e2caffbcfb373d951388a0f3f82f9b31
Morteza Araby <morteza.araby@outlook.com>
2016-10-05 15:07:06 +0200

    moving weather app to iOS folder

---
+++ iOS/rainyshinycloudy/rainyshinycloudy/WeatherVC.swift
@@ -0,0 +1,38 @@
+//
+//  WeatherVC.swift
+//  rainyshinycloudy
+//
+//  Created by Morteza Araby on 05/10/16.
+//  Copyright © 2016 Morteza Araby. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+}
+
