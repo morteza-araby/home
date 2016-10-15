880426c987f47a9fe873e15f55ddcd7f0232890f
Morteza Araby <morteza.araby@outlook.com>
2016-10-15 17:05:32 +0200

    Times Table uses slider and table view to show 1-20 multiplication table

---
+++ iOS/Times Tables/Times Tables/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Times Tables
+//
+//  Created by Morteza Araby on 2016-10-15.
+//  Copyright © 2016 Morteza Araby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func sliderChanged(_ sender: AnyObject) {
+        print(slider.value)
+        table.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String((Int(slider.value * 20)) * (indexPath.row + 1))
+        
+        return cell
+    }
+   
+}
+
